8e9aa6b3085411d313f4a62f906e8be4620d8367
dhruvsaikia <dhruvsaikia@gmail.com>
2024-05-23 02:47:56 -0400

    feat: elasticity using configurable joint and line renderer
    added basic elasticity using configurable joint & line renderer to show the elastic between the two players.

---
+++ Assets/Game/Scripts/Elastic/ElasticVisual.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Scripts.Elastic
+{
+    public class ElasticVisual : MonoBehaviour
+    {
+        public Transform player1;
+        public Transform player2;
+        private LineRenderer lineRenderer;
+
+        void Start()
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.positionCount = 2;
+            lineRenderer.startWidth = 0.1f;
+            lineRenderer.endWidth = 0.1f;
+            lineRenderer.material = new Material(Shader.Find("Sprites/Default")) { color = Color.red };
+        }
+
+        void Update()
+        {
+            lineRenderer.SetPosition(0, player1.position);
+            lineRenderer.SetPosition(1, player2.position);
+        }
+    }
+}
