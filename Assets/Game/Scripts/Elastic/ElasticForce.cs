ba3d1d5aa046b997b8afaf2b9d3a6bf329c8b782
dhruvsaikia <85579996+dhruvsaikia@users.noreply.github.com>
2024-05-26 20:43:09 -0400

    feat: add (better) elastic force (#30)
    * feat: initial elasticity mechanic
    
    one way of doing the elastic, somewhat working, but no yanking of players when they stretch elastic and let it go.
    
    * feat: different version of elastic
    
    a different version of elastic using animation curve and forces
    
    * some commit
    
     some commit
    
    * elsatic 2.0
    
    added few changes, 1500 ideal force range, ideal force multiplier - 0.3 - force applied = range where force starts getting applied
    
    * feat: okayish elsatic mechanic
    
    elastic with kind of working snapback, variables in the inspector to deal with the snapping force etc - current ideal snap force = 0.03
    
    * very minor changes
    
    random changes to 1-2 lines of code, not important
    
    * feat: edited elastic force
    
    More variables to play around with, clipping fixed, ideal values: Base Force: 1500, Snapback force : 150, Force Multiplier 1, 0.1, 1

---
+++ Assets/Game/Scripts/Elastic/ElasticForce.cs
@@ -0,0 +1,108 @@
+using Game.Scripts;
+using UnityEngine;
+
+
+namespace Game.Scripts.Elastic
+{
+    public class ElasticForce : MonoBehaviour
+    {
+        [SerializeField] private Transform player1;
+        [SerializeField] private Transform player2;
+        [SerializeField] private float baseForce = 1500;
+        [SerializeField] private float maxDistance = 10;
+        [SerializeField] private float maxForce = 100000;
+        [SerializeField] private AnimationCurve forceCurve;
+        [SerializeField, Range(0, 1)] private float forceApplied = 0.5f;
+        [SerializeField, Range(0, 1)] private float snapbackThreshold = 0.9f;
+        [SerializeField] private float snapbackForceMagnitude = 150;
+        [SerializeField, Range(0, 1)] private float midpointAdjustment = 0.5f;
+        [SerializeField] private Vector3 forceMultiplier = new(1, 0.1f, 1);
+
+        private Rigidbody _rb1;
+        private Rigidbody _rb2;
+        private PlayerController _controller1;
+        private PlayerController _controller2;
+
+        public Transform Player1 => player1;
+        public Transform Player2 => player2;
+        public float ForceAppliedDistance => forceApplied * maxDistance;
+        public float SnapbackDistance => snapbackThreshold * maxDistance;
+        public float MaxDistance => maxDistance;
+
+
+        private void Start()
+        {
+            _rb1 = player1.GetComponent<Rigidbody>();
+            _rb2 = player2.GetComponent<Rigidbody>();
+            _controller1 = player1.GetComponent<PlayerController>();
+            _controller2 = player2.GetComponent<PlayerController>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (player1 is null || player2 is null || _controller1 is null || _controller2 is null) return;
+
+            Vector3 midpoint = Vector3.Lerp(player1.position, player2.position, midpointAdjustment);
+
+            float distance1 = (player1.position - midpoint).magnitude;
+            float distance2 = (player2.position - midpoint).magnitude;
+
+            float normalizedDistance1 = distance1 / (maxDistance * midpointAdjustment);
+            float normalizedDistance2 = distance2 / (maxDistance * (1 - midpointAdjustment));
+
+            float forceMultiplier1 = forceCurve.Evaluate(normalizedDistance1);
+            float forceMultiplier2 = forceCurve.Evaluate(normalizedDistance2);
+
+            float forceMagnitude1 = Mathf.Min(forceMultiplier1 * baseForce, maxForce);
+            float forceMagnitude2 = Mathf.Min(forceMultiplier2 * baseForce, maxForce);
+
+
+            if (normalizedDistance1 > forceApplied)
+            {
+                Vector3 force1 = Vector3.Scale((midpoint - player1.position).normalized * forceMagnitude1, forceMultiplier);
+                _rb1.AddForce(force1);
+            }
+
+            if (normalizedDistance2 > forceApplied)
+            {
+                Vector3 force2 = Vector3.Scale((midpoint - player2.position).normalized * forceMagnitude2, forceMultiplier);
+                _rb2.AddForce(force2);
+            }
+
+            // Debug log for verification
+            Debug.Log($"Player1 - Distance: {distance1}, Normalized Distance: {normalizedDistance1}, Force Magnitude: {forceMagnitude1}");
+            Debug.Log($"Player2 - Distance: {distance2}, Normalized Distance: {normalizedDistance2}, Force Magnitude: {forceMagnitude2}");
+
+            AdjustAccelerationFactor(_controller1, normalizedDistance1);
+            AdjustAccelerationFactor(_controller2, normalizedDistance2);
+
+            ApplySnapbackForce(player1, _rb1, _controller1, normalizedDistance1, midpoint);
+            ApplySnapbackForce(player2, _rb2, _controller2, normalizedDistance2, midpoint);
+        }
+
+        private void ApplySnapbackForce(Transform player, Rigidbody rb, PlayerController controller, float normalizedDistance, Vector3 midpoint)
+        {
+            if (normalizedDistance >= snapbackThreshold)
+            {
+                Debug.Log($"{player.name} is in the snapback range");
+                if (controller.Movement == Vector3.zero)
+                {
+                    Vector3 snapbackForce = (midpoint - player.position).normalized * snapbackForceMagnitude;
+                    rb.AddForce(snapbackForce, ForceMode.Impulse);
+                    Debug.Log($"Snapback Force Applied to {player.name}: {snapbackForce}");
+                }
+            }
+        }
+        private void AdjustAccelerationFactor(PlayerController controller, float normalizedDistance)
+        {
+            if (normalizedDistance > 0.99)
+            {
+                controller.accelerationFactor = 0.5f; 
+            }
+            else
+            {
+                controller.accelerationFactor = 1f; 
+            }
+        }
+    }
+}
--- Assets/Game/Scripts/Elastic/ElasticVisual.cs
+++ Assets/Game/Scripts/Elastic/ElasticVisual.cs
@@ -1,29 +1,61 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Scripts.Elastic
 {
+    [RequireComponent(typeof(ElasticForce), typeof(LineRenderer))]
     public class ElasticVisual : MonoBehaviour
     {
-        public Transform player1;
-        public Transform player2;
-        private LineRenderer lineRenderer;
+        [SerializeField] private Material ghostMaterial;
+        [SerializeField] private float lineWidth = 0.1f;
+        [SerializeField] private bool useDebugColors = true;
 
-        void Start()
+        private ElasticForce _elasticForce;
+        private LineRenderer _lineRenderer;
+
+        private void Start()
         {
-            lineRenderer = GetComponent<LineRenderer>();
-            lineRenderer.positionCount = 2;
-            lineRenderer.startWidth = 0.1f;
-            lineRenderer.endWidth = 0.1f;
-            lineRenderer.material = new Material(Shader.Find("Sprites/Default")) { color = Color.red };
+            _elasticForce = GetComponent<ElasticForce>();
+            _lineRenderer = GetComponent<LineRenderer>();
+            _lineRenderer.positionCount = 2;
+            _lineRenderer.startWidth = lineWidth;
+            _lineRenderer.endWidth = lineWidth;
+
+            if (ghostMaterial)
+            {
+                _lineRenderer.material = ghostMaterial;
+            }
         }
 
-        void Update()
+        private void Update()
         {
-            lineRenderer.SetPosition(0, player1.position);
-            lineRenderer.SetPosition(1, player2.position);
+            var player1 = _elasticForce.Player1;
+            var player2 = _elasticForce.Player2;
+
+            if (player1 is null || player2 is null) return;
+
+            _lineRenderer.SetPosition(0, player1.position);
+            _lineRenderer.SetPosition(1, player2.position);
+
+            if (!useDebugColors) return;
+
+            Vector3 direction = player2.position - player1.position;
+            float distance = direction.magnitude;
+
+            if (distance <= _elasticForce.ForceAppliedDistance)
+            {
+                _lineRenderer.startColor = Color.green;
+                _lineRenderer.endColor = Color.green;
+            }
+            else if (distance <= _elasticForce.SnapbackDistance)
+            {
+                _lineRenderer.startColor = Color.yellow;
+                _lineRenderer.endColor = Color.yellow;
+            }
+            else
+            {
+                _lineRenderer.startColor = Color.red;
+                _lineRenderer.endColor = Color.red;
+            }
         }
     }
 }
