30487ee6eb34916a89c269e42e3ced18921c3d38
Kai Yiu Shum <132119893+SKYKIDKID@users.noreply.github.com>
2024-05-28 17:25:10 -0400

    feat: add cinemachine state change

---
+++ Assets/Game/Scripts/Utils/CinemachineSwitcher.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game.Scripts.Utils
+{
+    public class CinemachineSwitcher : MonoBehaviour
+    {
+        [Header("Callbacks")]
+        public UnityEvent onEnter;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out PlayerController player))
+            {
+                OnEnter();
+            }
+        }
+
+        private void OnEnter()
+        {
+            onEnter.Invoke();
+        }
+
+    }
+}
