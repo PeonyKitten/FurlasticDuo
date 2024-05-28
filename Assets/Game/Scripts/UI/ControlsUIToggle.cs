98f17331aadafd8f58463936531c5f7e51204330
Alvin Philips <alvinphilips257@gmail.com>
2024-05-27 23:35:39 -0400

    feat: add toggle for controls hint UI

---
+++ Assets/Game/Scripts/UI/ControlsUIToggle.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game.Scripts.UI
+{
+    public class ControlsUIToggle : MonoBehaviour
+    {
+        [SerializeField] private GameObject controls;
+        [SerializeField] private InputAction toggleControlsInputAction;
+
+        private bool _uiEnabled = true;
+        
+        private void Start()
+        {
+            toggleControlsInputAction.Enable();
+            toggleControlsInputAction.performed += ToggleUIControls;
+        }
+
+        private void ToggleUIControls(InputAction.CallbackContext obj)
+        {
+            _uiEnabled = !_uiEnabled;
+            controls.SetActive(_uiEnabled);
+        }
+
+        private void OnDestroy()
+        {
+            toggleControlsInputAction.performed -= ToggleUIControls;
+        }
+    }
+}
