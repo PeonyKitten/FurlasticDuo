a5fda4c7ec3a6b6c3e72a6f63a7754c7fbaf4d0e
Kai Yiu Shum <132119893+SKYKIDKID@users.noreply.github.com>
2024-05-31 18:12:43 -0400

    feat: add Playground 04 (#52)
    * temp prop: groundVent/FlowerPot
    
    * feat: add playground-04
    
    ---------
    
    Co-authored-by: John46L <john46infl@gmail.com>

--- Assets/Game/Scripts/Levels/KillFloor.cs
+++ Assets/Game/Scripts/Levels/KillFloor.cs
@@ -1,16 +1,20 @@
 using System;
 using Game.Scripts.Levels.Checkpoints;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Game.Scripts.Levels
 {
     public class KillFloor : MonoBehaviour
     {
+        public UnityEvent onKill;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent(out PlayerController _))
             {
                 CheckpointSystem.Instance.Respawn();
+                onKill?.Invoke();
             }
         }
     }
