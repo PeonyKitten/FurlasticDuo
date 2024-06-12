d8fce4e558c828f21b80576d65cc67662a50f636
Alvin Philips <alvinphilips257@gmail.com>
2024-06-11 22:47:02 -0400

    feat: floor grab

--- Assets/Game/Scripts/Grab/Grabbing.cs
+++ Assets/Game/Scripts/Grab/Grabbing.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace Game.Scripts.Grab
 {
@@ -7,14 +8,18 @@ namespace Game.Scripts.Grab
     public class Grabbing : MonoBehaviour
     {
         [SerializeField] private Vector3 objectCheckOffset = Vector3.zero;
-        //[SerializeField] private float grabRange = 2f; not  needed as we have sphereradius and object offset
-        [SerializeField] private float playerSpeedModifier = 0.5f;
+        [SerializeField] private float playerSpeedModifier = 1f;
         [SerializeField] private bool holdToGrab = true;
 
         [SerializeField] private Material selectedMaterial;
         [SerializeField] private Material defaultMaterial;
         [SerializeField] private float sphereRadius = 0.6f;
-        [SerializeField] private LayerMask layerMask = ~0;
+        [SerializeField] private LayerMask grabLayerMask = ~0;
+
+        [Header("Floor Grab Settings")]
+        [SerializeField] private bool groundGrab = true;
+        [SerializeField] private LayerMask groundGrabLayerMask;
+        [SerializeField] private float groundGrabDistance = 1f;
 
         private IGrabbable _currentGrabbable;
         private Transform _grabPoint;
@@ -23,6 +28,7 @@ namespace Game.Scripts.Grab
 
         private PlayerInput _playerInput;
         private InputAction _grabAction;
+        private readonly Collider[] _colliders = new Collider[10];
 
         public bool IsGrabbing { get; private set; } 
 
@@ -83,37 +89,51 @@ namespace Game.Scripts.Grab
         {
             var startPos = transform.position + objectCheckOffset;
 
-            var colliders = Physics.OverlapSphere(startPos, sphereRadius, layerMask, QueryTriggerInteraction.Ignore);
-            foreach (var collider in colliders)
+            var size = Physics.OverlapSphereNonAlloc(startPos, sphereRadius, _colliders, grabLayerMask.value, QueryTriggerInteraction.Ignore);
+            for (var index = 0; index < size; index++)
             {
-                if (collider.TryGetComponent(out IGrabbable grabbable))
+                if (FindAndGrab(_colliders[index])) return;
+            }
+            
+            // Fallback to ground grab
+            if (!groundGrab) return;
+
+            var groundRay = new Ray(transform.position, Vector3.down);
+            if (Physics.Raycast(groundRay, out var groundHit, groundGrabDistance, groundGrabLayerMask,
+                    QueryTriggerInteraction.Ignore))
+            {
+                FindAndGrab(groundHit.collider);
+            }
+        }
+
+        private bool FindAndGrab(Collider otherCollider)
+        {
+            if (!otherCollider.TryGetComponent(out IGrabbable grabbable)) return false;
+            
+            _currentGrabbable = grabbable;
+            _currentGrabbableObject = otherCollider.gameObject;
+            _currentGrabbable.OnGrab(_grabPoint);
+
+            AdjustPlayerSpeed();
+
+            var renderers = _currentGrabbableObject.GetComponentsInChildren<MeshRenderer>();
+            foreach (var mRenderer in renderers)
+            {
+                foreach (var rendererMaterial in mRenderer.sharedMaterials)
                 {
-                    _currentGrabbable = grabbable;
-                    _currentGrabbableObject = collider.gameObject;
-                    _currentGrabbable.OnGrab(_grabPoint);
-
-                    AdjustPlayerSpeed();
-
-                    var renderers = _currentGrabbableObject.GetComponentsInChildren<MeshRenderer>();
-                    foreach (var mRenderer in renderers)
-                    {
-                        foreach (var rendererMaterial in mRenderer.sharedMaterials)
-                        {
-                            if (rendererMaterial != defaultMaterial) continue;
-                            mRenderer.material = selectedMaterial;
-                        }
-                    }
-                    
-                    IsGrabbing = grabbable.ShouldAffectElastcForce();
-
-                    return;
+                    if (rendererMaterial != defaultMaterial) continue;
+                    mRenderer.material = selectedMaterial;
                 }
             }
+                    
+            IsGrabbing = grabbable.ShouldAffectElastcForce();
+
+            return true;
         }
 
         private void Release()
         {
-            if (_currentGrabbable == null) return;
+            if (_currentGrabbableObject is null) return;
 
             _currentGrabbable.OnRelease(_grabPoint);
 
@@ -157,6 +177,11 @@ namespace Game.Scripts.Grab
             Gizmos.color = Color.blue;
             var startPos = transform.position + objectCheckOffset;
             Gizmos.DrawWireSphere(startPos, sphereRadius);
+
+            if (groundGrab)
+            {
+                Gizmos.DrawRay(transform.position, Vector3.down * groundGrabDistance);
+            }
         }
     }
 }
