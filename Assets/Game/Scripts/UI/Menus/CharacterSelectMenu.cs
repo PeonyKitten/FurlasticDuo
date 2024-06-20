20871662f0a9fa05b652789b846a64680bb99e4e
Alvin Philips <alvinphilips257@gmail.com>
2024-06-20 14:20:21 -0400

    clean: alvin can't spell character

--- Assets/Game/Scripts/UI/Menus/CharacterSelectMenu.cs
+++ Assets/Game/Scripts/UI/Menus/CharacterSelectMenu.cs
@@ -1,8 +1,36 @@
+using System.Collections;
+using Game.Scripts.Utils;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 namespace Game.Scripts.UI.Menus
 {
-    public class CharacterSelectMenu : MonoBehaviour
+    public class CharacterSelectMenu : Menu
     {
+        public void OnPlayerJoined(PlayerInput player)
+        {
+            Debug.Log(player.devices[0].displayName);
+        }
+
+        public void OnPlayerLeft(PlayerInput player)
+        {
+            Debug.Log("Goodbye, " + player.devices[0].displayName);
+        }
+        
+        protected override IEnumerator Generate()
+        {
+            yield return null;
+            
+            var root = Document.rootVisualElement;
+            root.styleSheets.Add(style);
+
+            var deviceList = root.Q("InputDeviceList");
+
+            foreach (var device in Gamepad.all)
+            {
+                deviceList.Create<Label>().text = device.displayName;
+            }
+        }
     }
 }
