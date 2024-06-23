78b22f2fc723f709d0ba52c45d13887707ba493b
Alvin Philips <alvinphilips257@gmail.com>
2024-06-22 20:35:34 -0400

    feat: add utility script to do something when an event occurs

---
+++ Assets/Game/Scripts/Misc/DoSomethingOnEvent.cs
@@ -0,0 +1,73 @@
+using System;
+using Game.Scripts.Player;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game.Scripts.Misc
+{
+    public class DoSomethingOnEvent : MonoBehaviour
+    {
+        [Serializable]
+        public enum ListenableEvent
+        {
+            None,
+            ObjectStart,
+            ObjectDestroy,
+            TriggerEnter,
+            TriggerExit,
+            CollisionEnter,
+            CollisionExit,
+        }
+
+        [SerializeField] private ListenableEvent performActionOnEvent = ListenableEvent.None;
+        [SerializeField] private bool checkIfPlayer = true;
+
+        public UnityEvent onAction;
+
+        private void Start()
+        {
+            if (performActionOnEvent != ListenableEvent.ObjectStart) return;
+            
+            onAction?.Invoke();
+        }
+        
+        private void OnDestroy()
+        {
+            if (performActionOnEvent != ListenableEvent.ObjectDestroy) return;
+            
+            onAction?.Invoke();
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (performActionOnEvent != ListenableEvent.TriggerEnter) return;
+            if (checkIfPlayer && !other.gameObject.TryGetComponent(out PlayerController _)) return;
+            
+            onAction?.Invoke();
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            if (performActionOnEvent != ListenableEvent.TriggerExit) return;
+            if (checkIfPlayer && !other.gameObject.TryGetComponent(out PlayerController _)) return;
+            
+            onAction?.Invoke();
+        }
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            if (performActionOnEvent != ListenableEvent.CollisionEnter) return;
+            if (checkIfPlayer && !other.gameObject.TryGetComponent(out PlayerController _)) return;
+            
+            onAction?.Invoke();
+        }
+        
+        private void OnCollisionExit(Collision other)
+        {
+            if (performActionOnEvent != ListenableEvent.CollisionExit) return;
+            if (checkIfPlayer && !other.gameObject.TryGetComponent(out PlayerController _)) return;
+            
+            onAction?.Invoke();
+        }
+    }
+}
