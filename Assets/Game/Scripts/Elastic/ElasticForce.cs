eaf1b8517d8e730bdee67b01c852aeaf4971bb97
dhruvsaikia <dhruvsaikia@gmail.com>
2024-06-30 21:43:20 -0400

    feat: force by duration and distance, removed impulses
    Added enum to choose between applying force by distance or duration.
    
    Replaced ApplySnapbackForce with ShouldApplySnapback. Same as old method, but doesnt apply force directly.
    
    Added ContinueSnapbackForce, handles continous application of snapback force and supports both duration and distance based snapback modes.

--- Assets/Game/Scripts/Elastic/ElasticForce.cs
+++ Assets/Game/Scripts/Elastic/ElasticForce.cs
@@ -1,6 +1,6 @@
 // ElasticForce.cs
 // Dhruv Saikia, Alvin Philips
-// 2024-06-21
+// 2024-06-30
 // Controls the elastic force between the players, along with snapping back. 
 
 using Game.Scripts;
@@ -13,26 +13,36 @@ namespace Game.Scripts.Elastic
 {
     public class ElasticForce : MonoBehaviour
     {
+
+        [System.Serializable]
+        public enum SnapbackMode
+        {
+            Duration,
+            Distance
+        }
+
+
         [Header("Anchor settings")]
         [SerializeField] private Transform player1;
         [SerializeField] private Transform player2;
         [SerializeField, Range(0, 1)] private float midpointAdjustment = 0.5f;
         [SerializeField] private float maxDistance = 10;
-        
-        // Forces to be applied. forceApplied is the normalized distance of the maxDistance where it will continously apply a force
+
         [Header("Force Settings")]
         [SerializeField] private float baseForce = 1500;
         [SerializeField] private float maxForce = 100000;
         [SerializeField] private AnimationCurve forceCurve;
         [SerializeField, Range(0, 1)] private float forceApplied = 0.5f;
         [SerializeField] private AnimationCurve yAxisForceFactorDot;
-        
-        
+
+
         [Header("Snapback Settings")]
+        [SerializeField] private SnapbackMode snapbackMode = SnapbackMode.Duration;
         [SerializeField, Range(0, 1)] private float snapbackThreshold = 0.9f;
         [SerializeField] private float snapbackForceMagnitude = 150;
         [SerializeField] private float snapbackDelay = 0.5f;
-        
+        [SerializeField] private float snapbackDuration = 0.5f;
+
         [Header("Rumble Settings")]
         [SerializeField] private bool useRumble = true;
         [SerializeField, Range(0, 1)] private float rumbleAmount = 0.5f;
@@ -43,13 +53,14 @@ namespace Game.Scripts.Elastic
         private PlayerController _controller1;
         private PlayerController _controller2;
         private float _snapbackTimer;
+        private float _snapbackDurationTimer;
+        private bool _isApplyingSnapback;
 
         public Transform Player1 => player1;
         public Transform Player2 => player2;
         public float ForceAppliedDistance => forceApplied * maxDistance;
         public float SnapbackDistance => snapbackThreshold * maxDistance;
         public float MaxDistance => maxDistance;
-
 
         private void Start()
         {
@@ -62,7 +73,7 @@ namespace Game.Scripts.Elastic
         private void FixedUpdate()
         {
             _snapbackTimer -= Time.deltaTime;
-            
+
             if (player1 is null || player2 is null || _controller1 is null || _controller2 is null) return;
 
             Vector3 midpoint = Vector3.Lerp(player1.position, player2.position, midpointAdjustment);
@@ -72,11 +83,10 @@ namespace Game.Scripts.Elastic
 
             float normalizedDistance1 = distance1 / (maxDistance * midpointAdjustment);
             float normalizedDistance2 = distance2 / (maxDistance * (1 - midpointAdjustment));
-            
-            // Use Force Applied factor to calculate forceCurve contribution
+
             const float forceAppliedAdjustmentFactor = 0.05f;
             var adjustedForceApplied = forceApplied - forceAppliedAdjustmentFactor;
-            
+
             var forceAppliedDistance1 = (normalizedDistance1 - adjustedForceApplied) / (1f - adjustedForceApplied);
             var forceAppliedDistance2 = (normalizedDistance2 - adjustedForceApplied) / (1f - adjustedForceApplied);
 
@@ -85,7 +95,7 @@ namespace Game.Scripts.Elastic
 
             float forceMagnitude1 = Mathf.Min(forceMultiplier1 * baseForce, maxForce);
             float forceMagnitude2 = Mathf.Min(forceMultiplier2 * baseForce, maxForce);
-            
+
             if (CalculateBindingForce(normalizedDistance1, player1, midpoint, forceMagnitude1, out var force1, forceAppliedDistance1))
             {
                 _rb1.AddForce(force1);
@@ -95,19 +105,26 @@ namespace Game.Scripts.Elastic
             {
                 _rb2.AddForce(force2);
             }
-            
+
             AdjustAccelerationFactor(_controller1, normalizedDistance1);
             AdjustAccelerationFactor(_controller2, normalizedDistance2);
 
             if (_snapbackTimer > 0) return;
 
-            var snapbackPlayer1 = ApplySnapbackForce(player1, _rb1, _controller1, normalizedDistance1, midpoint);
-            var snapbackPlayer2 = ApplySnapbackForce(player2, _rb2, _controller2, normalizedDistance2, midpoint);
+            var snapbackPlayer1 = ShouldApplySnapback(_controller1, normalizedDistance1);
+            var snapbackPlayer2 = ShouldApplySnapback(_controller2, normalizedDistance2);
 
             // The OR here is used to prevent lazy evaluation of the ApplySnapbackForce
             if (snapbackPlayer1 || snapbackPlayer2)
             {
                 _snapbackTimer = snapbackDelay;
+                _snapbackDurationTimer = snapbackDuration;
+                _isApplyingSnapback = true;
+            }
+
+            if (_isApplyingSnapback)
+            {
+                ContinueSnapbackForce(snapbackPlayer1, snapbackPlayer2, normalizedDistance1, normalizedDistance2);
             }
         }
 
@@ -125,36 +142,69 @@ namespace Game.Scripts.Elastic
                 var rumble = rumbleForceCurve.Evaluate(forceAppliedNormalizedDistance) * rumbleAmount;
                 Gamepad.current.SetMotorSpeeds(rumble, rumble);
             }
-            
+
             force = (midpoint - player.position).normalized * forceMagnitude;
             var yFactor = Mathf.Abs(Vector3.Dot(force.normalized, Vector3.up));
             force.y *= yAxisForceFactorDot.Evaluate(yFactor);
-            
+
             return true;
         }
 
-        private bool ApplySnapbackForce(Transform player, Rigidbody rb, PlayerController controller, float normalizedDistance, Vector3 midpoint)
+        private bool ShouldApplySnapback(PlayerController controller, float normalizedDistance)
         {
             if (normalizedDistance < snapbackThreshold) return false;
             if (controller.IsGrabbing || controller.Movement != Vector3.zero) return false;
-            
-            Vector3 snapbackForce = (midpoint - player.position).normalized * snapbackForceMagnitude;
-            rb.AddForce(snapbackForce, ForceMode.Impulse);
 
             return true;
         }
-        
+
+        private void ContinueSnapbackForce(bool applyToPlayer1, bool applyToPlayer2, float normalizedDistance1, float normalizedDistance2)
+        {
+            if (snapbackMode == SnapbackMode.Duration)
+            {
+                _snapbackDurationTimer -= Time.fixedDeltaTime;
+                if (_snapbackDurationTimer <= 0)
+                {
+                    _isApplyingSnapback = false;
+                    return;
+                }
+            }
+
+            if (applyToPlayer1) ApplySnapbackForceToPlayer(_rb1, player1.position, normalizedDistance1);
+            if (applyToPlayer2) ApplySnapbackForceToPlayer(_rb2, player2.position, normalizedDistance2);
+        }
+
+        private void ApplySnapbackForceToPlayer(Rigidbody rb, Vector3 playerPosition, float normalizedDistance)
+        {
+            Vector3 midpoint = Vector3.Lerp(player1.position, player2.position, midpointAdjustment);
+            Vector3 toMidpoint = midpoint - playerPosition;
+
+            bool shouldApplyForce = snapbackMode == SnapbackMode.Duration ||
+                                    (snapbackMode == SnapbackMode.Distance && normalizedDistance > snapbackThreshold);
+
+            if (shouldApplyForce)
+            {
+                Vector3 snapbackForce = toMidpoint.normalized * snapbackForceMagnitude;
+                rb.AddForce(snapbackForce, ForceMode.Force);
+            }
+            else if (snapbackMode == SnapbackMode.Distance)
+            {
+                _isApplyingSnapback = false;
+            }
+        }
+
         private void AdjustAccelerationFactor(PlayerController controller, float normalizedDistance)
         {
-            //To prevent players from moving further than max distance (TO BE CHANGED TO A BETTER METHOD)
             if (normalizedDistance > 0.99)
             {
-                controller.accelerationFactor = 0.5f; 
+                controller.accelerationFactor = 0.5f;
             }
             else
             {
-                controller.accelerationFactor = 1f; 
+                controller.accelerationFactor = 1f;
             }
         }
     }
 }
+
+
