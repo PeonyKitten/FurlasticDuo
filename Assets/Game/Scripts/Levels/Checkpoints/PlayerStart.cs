b147a19dba974807b78d252328324991fd993c6c
Alvin Philips <alvinphilips257@gmail.com>
2024-07-13 19:16:38 -0400

    feat: add PlayerStart

---
+++ Assets/Game/Scripts/Levels/Checkpoints/PlayerStart.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Scripts.Levels.Checkpoints
+{
+    public class PlayerStart : Checkpoint
+    {
+        protected override void Start()
+        {
+            base.Start();
+            
+            CheckpointSystem.Instance.SaveCheckpoint(this);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawIcon(transform.position, "Game/Scripts/Levels/Checkpoints/PlayerStart_Players", true);
+        }
+    }
+}
